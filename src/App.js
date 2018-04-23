78fce6240be1f9f37b91be9ea5468e671906c550
rmartynenko <rmartynenko@mobiledimension.ru>
2018-04-23 15:34:16 +0300

    adds ability to create todos

--- src/App.js
+++ src/App.js
@@ -1,5 +1,5 @@
-import React, { Component } from "react";
-import db from "./mockDB";
+import React, { Component } from 'react';
+import db from './mockDB';
 
 const toArray = obj => Object.keys(obj).map(key => ({ ...obj[key], id: key }));
 const sortByTs = todos => toArray(todos).sort((a, b) => b.ts - a.ts);
@@ -8,19 +8,33 @@ class App extends Component {
   state = {
     todos: {},
     todoList: [],
-    loaded: false
+    loaded: false,
+    newTitle: '',
   };
 
   actions = {
+    changeTitleInput: title => {
+      this.setState({
+        newTitle: title,
+      });
+    },
+    submit: () => {
+      db.addTodo({
+        title: this.state.newTitle,
+      });
+      this.setState({
+        newTitle: '',
+      });
+    },
     syncTodos: () => {
       db.syncTodos(todos => {
         this.setState({
           todos,
           loaded: true,
-          todoList: sortByTs(todos)
+          todoList: sortByTs(todos),
         });
       });
-    }
+    },
   };
 
   componentDidMount() {
@@ -34,18 +48,37 @@ class App extends Component {
     return (
       <div>
         <h1>Todos</h1>
+        <Form {...this.state} {...this.actions} />
         <Todos {...this.state} />
       </div>
     );
   }
 }
 
+const Form = ({ newTitle, changeTitleInput, submit }) => (
+  <form
+    onSubmit={e => {
+      e.preventDefault();
+      submit();
+    }}
+  >
+    <input
+      type="text"
+      value={newTitle}
+      onChange={e => {
+        changeTitleInput(e.target.value);
+      }}
+    />
+    <button disabled={!newTitle}>Submit</button>
+  </form>
+);
+
 const Todos = props => {
   return (
     <div>
       {props.todoList.map(todo => <div key={todo.id}>{todo.title}</div>)}
     </div>
-    );
+  );
 };
 
 export default App;
